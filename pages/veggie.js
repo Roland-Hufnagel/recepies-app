7a3fc0f049ed34817b7efa8d7ae9446a2e22a080
Roland-Hufnagel <roland.hufnagel@gmx.de>
2022-10-05 08:45:47 +0200

    add pizza img to veggie-site

--- pages/veggie.js
+++ pages/veggie.js
@@ -1,13 +1,22 @@
 import styled from "styled-components";
 import Head from "next/head";
-
+import Image from "next/image";
 
 export default function Veggie() {
   return (
     <>
-    <Head><title>Nice Vegies</title></Head>
-      <h1>Veggie recipies</h1>
+    <Head><title>Nice Vegies</title></Head> 
+    <h1>Veggie recipies</h1>
       <p>more than just food</p>
+    
+    <Image 
+    src="/img/pizza.jpg"
+    alt="Pizza"
+    layout="responsive"
+    width={2400}
+    height={1596}
+    ></Image>
+      
     </>
   );
 }
